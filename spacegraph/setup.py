5cdff9e17e1212ca391d3ba357a3f7b1d202f7ef
Gengchen Mai <gengchen.mai@gmail.com>
2020-02-14 10:23:41 -0800

    add reuirements.txt, modify readme.md

---
+++ spacegraph/setup.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+with open('../readme.md') as f:
+    readme = f.read()
+
+with open('../LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    reqs = f.read()
+
+setup(
+    name='space2vec',
+    version='0.1.0',
+    description='Multi-Scale Representation Learning for Spatial Feature Distributions using Grid Cells',
+    long_description=readme,
+    license=license,
+    python_requires='==2.7',
+    packages=find_packages(exclude=('examples')),
+    install_requires=reqs.strip().split('\n'),
+)
